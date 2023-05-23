2494cf06bddab3e9a3b432ca888334b305ae04da
Anvin.C.Shaju <anvincsg@gmail.com>
2023-05-23 10:59:04 +0530

    updated the tweet button

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -4,7 +4,7 @@ const Buttons = ({showNewQuote}) => {
     return (
         
         <div id="buttons">
-        <a href="twitter.com/intent/tweet" id="tweet-quote" target="_blank" className="button" title="Tweet this quote!" >
+        <a href="https://twitter.com/intent/tweet" id="tweet-quote" target="_blank" className="button" title="Tweet this quote!" >
             Tweet
         </a>
         <button id="new-quote" className="button" onClick={showNewQuote} >New Quote</button>
